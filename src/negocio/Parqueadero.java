b003c6b035187ce2b7895f57fa482260540982d8
Emanuel <emartinezpinzon@gmail.com>
2015-05-02 03:54:20 -0500

    Cambio de prueba

--- src/negocio/Parqueadero.java
+++ src/negocio/Parqueadero.java
@@ -4,6 +4,48 @@ package negocio;
  * @author Emanuel Martínez Pinzón
  */
 
+import java.util.ArrayList;
+
 public class Parqueadero {
     
+    ArrayList<Puesto> Puesto;
+    ArrayList<Carro> Carro;
+    private int tarifaHora;
+    private int tarifaFraccion;
+    private int totalRecaudado;
+    
+    
+    public Parqueadero(){
+        this.tarifaHora = 1000;
+        this.tarifaFraccion = 500;
+    }
+    
+    private void crearPuestos(){
+        for(int i=0; i<4; i++)
+            this.Puesto.add(new Puesto(i+1));
+    }
+
+    public int getTarifaHora() {
+        return tarifaHora;
+    }
+
+    public void setTarifaHora(int tarifaHora) {
+        this.tarifaHora = tarifaHora;
+    }
+
+    public int getTarifaFraccion() {
+        return tarifaFraccion;
+    }
+
+    public void setTarifaFraccion(int tarifaFraccion) {
+        this.tarifaFraccion = tarifaFraccion;
+    }
+
+    public int getTotalRecaudado() {
+        return totalRecaudado;
+    }
+
+    public void setTotalRecaudado(int totalRecaudado) {
+        this.totalRecaudado = totalRecaudado;
+    }
 }
--- src/negocio/Puesto.java
+++ src/negocio/Puesto.java
@@ -6,4 +6,40 @@ package negocio;
 
 public class Puesto {
     
+    private int numero;
+    private String estado;
+    private Carro carro;
+    
+    public Puesto(int numero){
+        this.numero = numero;
+        this.estado = "Libre";
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+    
+    public String toString(){
+        return "";
+    }
 }
